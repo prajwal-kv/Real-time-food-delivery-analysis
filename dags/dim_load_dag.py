6129bdaa48fbac8374b3022188e66a7e5b528712
prajwal-kv <prajwalkv620@gmail.com>
2025-12-01 21:01:44 +0530

    Added DAGs, scripts, and table creation

--- dags/dim_load_dag.py
+++ dags/dim_load_dag.py
@@ -1,12 +1,8 @@
 from airflow import DAG
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-#from airflow.providers.postgres.operators.postgres import PostgresOperator
-#from airflow.utils.dates import days_ago
-from datetime import datetime, timedelta
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-
-#from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
@@ -21,49 +17,47 @@ with DAG(
     dag_id='create_and_load_dim',
     default_args=default_args,
     description='ETL for food delivery data into Redshift',
-    schedule=None,                      # <-- use `schedule`, not `schedule_interval`
+    schedule=None,
     start_date=datetime(2025, 11, 30),
     catchup=False,
 ) as dag:
 
-
-
     # Create schema if it doesn't exist
     create_schema = SQLExecuteQueryOperator(
         task_id='create_schema',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="CREATE SCHEMA IF NOT EXISTS food_delivery_datamart;",
     )
 
-    # Drop tables if they exist
+    # Drop existing tables
     drop_dimCustomers = SQLExecuteQueryOperator(
         task_id='drop_dimCustomers',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="DROP TABLE IF EXISTS food_delivery_datamart.dimCustomers;",
     )
 
     drop_dimRestaurants = SQLExecuteQueryOperator(
         task_id='drop_dimRestaurants',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="DROP TABLE IF EXISTS food_delivery_datamart.dimRestaurants;",
     )
 
     drop_dimDeliveryRiders = SQLExecuteQueryOperator(
         task_id='drop_dimDeliveryRiders',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="DROP TABLE IF EXISTS food_delivery_datamart.dimDeliveryRiders;",
     )
 
     drop_factOrders = SQLExecuteQueryOperator(
         task_id='drop_factOrders',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="DROP TABLE IF EXISTS food_delivery_datamart.factOrders;",
     )
 
     # Create dimension and fact tables
     create_dimCustomers = SQLExecuteQueryOperator(
         task_id='create_dimCustomers',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="""
             CREATE TABLE food_delivery_datamart.dimCustomers (
                 CustomerID INT PRIMARY KEY,
@@ -78,7 +72,7 @@ with DAG(
 
     create_dimRestaurants = SQLExecuteQueryOperator(
         task_id='create_dimRestaurants',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="""
             CREATE TABLE food_delivery_datamart.dimRestaurants (
                 RestaurantID INT PRIMARY KEY,
@@ -92,7 +86,7 @@ with DAG(
 
     create_dimDeliveryRiders = SQLExecuteQueryOperator(
         task_id='create_dimDeliveryRiders',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="""
             CREATE TABLE food_delivery_datamart.dimDeliveryRiders (
                 RiderID INT PRIMARY KEY,
@@ -107,7 +101,7 @@ with DAG(
 
     create_factOrders = SQLExecuteQueryOperator(
         task_id='create_factOrders',
-        postgres_conn_id='redshift_connection_id',
+        conn_id='redshift_connection_id',
         sql="""
             CREATE TABLE food_delivery_datamart.factOrders (
                 OrderID INT PRIMARY KEY,
@@ -129,8 +123,8 @@ with DAG(
         task_id='load_dimCustomers',
         schema='food_delivery_datamart',
         table='dimCustomers',
-        s3_bucket='food-delivery-data-analysis-one',# S3-bucket id
-        s3_key='dims/dimCustomers.csv', #S3 folder name inside that bucket
+        s3_bucket='food-delivery-data-analysis-one',
+        s3_key='dims/dimCustomers.csv',
         copy_options=['CSV', 'IGNOREHEADER 1', 'QUOTE as \'"\''],
         aws_conn_id='aws_default',
         redshift_conn_id='redshift_connection_id',
@@ -151,36 +145,32 @@ with DAG(
         task_id='load_dimDeliveryRiders',
         schema='food_delivery_datamart',
         table='dimDeliveryRiders',
-        s3_bucket='food-delivery-data-analysis-one', 
+        s3_bucket='food-delivery-data-analysis-one',
         s3_key='dims/dimDeliveryRiders.csv',
         copy_options=['CSV', 'IGNOREHEADER 1', 'QUOTE as \'"\''],
         aws_conn_id='aws_default',
         redshift_conn_id='redshift_connection_id',
     )
-    #used to triggere the next dag
+
+    # Trigger next DAG after loading dimensions
     trigger_spark_streaming_dag = TriggerDagRunOperator(
-    task_id="trigger_spark_streaming_dag",
-    trigger_dag_id="submit_pyspark_streaming_job_to_emr",
+        task_id="trigger_spark_streaming_dag",
+        trigger_dag_id="submit_pyspark_streaming_job_to_emr",
     )
 
-# First, create the schema,then running the multiple tasks in parallel to drop existing tables
-create_schema >> [drop_dimCustomers, drop_dimRestaurants, drop_dimDeliveryRiders, drop_factOrders]
+    # Task dependencies
+    create_schema >> [drop_dimCustomers, drop_dimRestaurants, drop_dimDeliveryRiders, drop_factOrders]
+    [drop_dimCustomers, drop_dimRestaurants, drop_dimDeliveryRiders] >> create_factOrders
 
-# Once the existing tables are dropped, proceed to create new tables
-drop_dimCustomers >> create_dimCustomers
-drop_dimRestaurants >> create_dimRestaurants
-drop_dimDeliveryRiders >> create_dimDeliveryRiders
-drop_factOrders >> create_factOrders
+    drop_dimCustomers >> create_dimCustomers
+    drop_dimRestaurants >> create_dimRestaurants
+    drop_dimDeliveryRiders >> create_dimDeliveryRiders
+    drop_factOrders >> create_factOrders
 
-#first create dimension tables then fact table as fact table depends on dimension tables
-[create_dimCustomers, create_dimRestaurants, create_dimDeliveryRiders] >> create_factOrders
+    [create_dimCustomers, create_dimRestaurants, create_dimDeliveryRiders] >> create_factOrders
 
-# After each table is created, load the corresponding data
-# only dimension tables are loaded from S3 in this DAG
-# only after loading the dimension tables the next dag is triggered
-# fact table will be loaded in the next dag using spark streaming job
-create_dimCustomers >> load_dimCustomers
-create_dimRestaurants >> load_dimRestaurants
-create_dimDeliveryRiders >> load_dimDeliveryRiders
+    create_dimCustomers >> load_dimCustomers
+    create_dimRestaurants >> load_dimRestaurants
+    create_dimDeliveryRiders >> load_dimDeliveryRiders
 
-[load_dimCustomers, load_dimRestaurants, load_dimDeliveryRiders] >> trigger_spark_streaming_dag
+    [load_dimCustomers, load_dimRestaurants, load_dimDeliveryRiders] >> trigger_spark_streaming_dag
